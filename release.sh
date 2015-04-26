c91fb0420424f889de6466a4b25fc7704e0ad153
markmal <markmal@github.com>
2015-04-26 12:01:57 -0400

    Whatsnew updated

---
+++ release.sh
@@ -0,0 +1,10 @@
+# Zips files for binary distribution
+BLDDT=$(strings FreeShip |grep Build:|cut -f2 -d' ')
+VERS=$(Utils/versinfo FreeShip|grep 'File Full Version:'|cut -f2)
+ARCH=$(file -b FreeShip|cut -f6 -d' '|sed 's/,//')
+
+git tag -a v$VERS -m "Test Release. $ARCH Linux Qt. ver $VERS"
+
+./zipbin
+
+

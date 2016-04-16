a848cb600e8cda35181c327423b933888d9c11e9
markmal <mark.malakanov@gmail.com>
2016-04-16 16:29:06 -0400

    improved versinfo

--- Utils/versinfo.pas
+++ Utils/versinfo.pas
@@ -1,9 +1,9 @@
 program versinfo;
-//Getting file version information
+// Getting file version information
 // slightly modified from fcl-res help file program res3 (mainly comments).
 // This program reads a supported resource file (and autodetects the resource type)
 // and prints out version info
-{$mode objfpc}
+//{$mode objfpc}
  
 uses
   Classes, SysUtils, resource, versionresource, versiontypes,
@@ -12,6 +12,13 @@ uses
   resreader, coffreader, elfreader, winpeimagereader,
   elfconsts;
  
+type
+  TOutSet = set of (
+    osFileFullVersion, osFileMajorVersion, osFileMinorVersion,
+    osFileRelease, osFileBuild,
+    osProductFullVersion, osProductMajorVersion, osProductMinorVersion,
+    osProductRelease, osProductBuild);
+
 var
   resources : TResources;
   inFile : TFileStream;
@@ -20,48 +27,162 @@ var
   vr : TVersionResource;
   fi : TVersionFixedInfo;
   fv : TFileProductVersion;
-  I,RC : integer;
+  F,I,RC : integer;
   fos : longword;
-begin
-  inFile:=TFileStream.Create(paramstr(1), fmOpenRead or fmShareDenyNone);
-  resources := TResources.Create;
-  reader:=resources.FindReader(inFile);
-  resources.LoadFromStream(inFile,reader);
+  outset : TOutSet;
+  p : string;
+  inFileNames : TStringList;
+  verbose: boolean;
 
-  RC := resources.Count;
-  for I:=0 to RC-1 do
+  procedure printHelp;
   begin
-    try
-     ar := resources[I];
-     if ar is TVersionResource then
-     begin
-       vr := TVersionResource(ar);
-       fi:= vr.FixedInfo;
-       writeln('File Full Version:' +#9+ IntToStr(fi.FileVersion[0])
-                          + '.' + IntToStr(fi.FileVersion[1])
-                          + '.' + IntToStr(fi.FileVersion[2])
-                          + '.' + IntToStr(fi.FileVersion[3]));
-       writeln('File Major Version:' +#9+ IntToStr(fi.FileVersion[0]));
-       writeln('File Minor Version:' +#9+ IntToStr(fi.FileVersion[1]));
-       writeln('File Release:' +#9+ IntToStr(fi.FileVersion[2]));
-       writeln('File Build:' +#9+ IntToStr(fi.FileVersion[3]));
-
-       writeln('Product Full Version:' +#9+ IntToStr(fi.ProductVersion[0])
-                          + '.' + IntToStr(fi.ProductVersion[1])
-                          + '.' + IntToStr(fi.ProductVersion[2])
-                          + '.' + IntToStr(fi.ProductVersion[3]));
-       writeln('Product Major Version:' +#9+ IntToStr(fi.ProductVersion[0]));
-       writeln('Product Minor Version:' +#9+ IntToStr(fi.ProductVersion[1]));
-       writeln('Product Release:' +#9+ IntToStr(fi.ProductVersion[2]));
-       writeln('Product Build:' +#9+ IntToStr(fi.ProductVersion[3]));
-     end;
-    finally
-    end;
+  writeln('versinfo - Extracts version info from binary executables');
+  writeln('Usage: versinfo [param] [param] ... [param]');
+  writeln('  where param is one of');
+  writeln('  --FileFullVersion');
+  writeln('  --FileMajorVersion');
+  writeln('  --FileMinorVersion');
+  writeln('  --FileRelease');
+  writeln('  --FileBuild');
+  writeln('  --ProductFullVersion');
+  writeln('  --ProductMajorVersion');
+  writeln('  --ProductMinorVersion');
+  writeln('  --ProductRelease');
+  writeln('  --ProductBuild');
+  writeln('  --NoVerbose');
+  writeln('  --help');
+  writeln('  <file name>');
+  writeln('Note: At least one file name must be specified');
+  writeln('      All will printed all when no options specified');
+  writeln('      NoVebose prints just value(s) without name(s)');
   end;
 
-  // clean up - adding try..except blocks would be nice:
-  resources.Free;
-  reader.Free;
-  inFile.Free;
+begin
+  if paramcount = 0 then printHelp;
+  outset := [];
+  inFileNames:=TStringList.Create;
+  verbose := true;
+
+  for I:=0 to paramcount do
+    begin
+    p:=paramstr(I);
+    if      p='--FileFullVersion' then outset := outset + [osFileFullVersion]
+    else if p='--FileMajorVersion' then outset := outset + [osFileMajorVersion]
+    else if p='--FileMinorVersion' then outset := outset + [osFileMinorVersion]
+    else if p='--FileRelease' then outset := outset + [osFileRelease]
+    else if p='--FileBuild' then outset := outset + [osFileBuild]
+    else if p='--ProductFullVersion' then outset := outset + [osProductFullVersion]
+    else if p='--ProductMajorVersion' then outset := outset + [osProductMajorVersion]
+    else if p='--ProductMinorVersion' then outset := outset + [osProductMinorVersion]
+    else if p='--ProductRelease' then outset := outset + [osProductRelease]
+    else if p='--ProductBuild' then outset := outset + [osProductBuild]
+    else if p='--NoVerbose' then verbose := false
+    else if p='--help' then begin printHelp; exit; end
+    else inFileNames.Add(p);
+    end;
+  if inFileNames.Count=0 then
+    begin writeln('File name not specified');
+    exitCode:=1; exit;
+    end;
+  if outset = [] then outset:=[osFileFullVersion, osFileMajorVersion, osFileMinorVersion,
+    osFileRelease, osFileBuild,
+    osProductFullVersion, osProductMajorVersion, osProductMinorVersion,
+    osProductRelease, osProductBuild];
+
+  for F:=0 to inFileNames.Count-1 do
+    begin
+    inFile:=TFileStream.Create(inFileNames[F], fmOpenRead or fmShareDenyNone);
+    resources := TResources.Create;
+    reader:=resources.FindReader(inFile);
+    resources.LoadFromStream(inFile,reader);
+
+    RC := resources.Count;
+    for I:=0 to RC-1 do
+      begin
+        try
+         ar := resources[I];
+         if ar is TVersionResource then
+         begin
+           vr := TVersionResource(ar);
+           fi:= vr.FixedInfo;
+
+           if [osFileFullVersion] in outset then
+             begin
+               if verbose then write('File Full Version:' +#9);
+               writeln(IntToStr(fi.FileVersion[0])
+                              + '.' + IntToStr(fi.FileVersion[1])
+                              + '.' + IntToStr(fi.FileVersion[2])
+                              + '.' + IntToStr(fi.FileVersion[3]));
+             end;
+
+           if [osFileMajorVersion] in outset then
+             begin
+               if verbose then write('File Major Version:' +#9);
+               writeln(IntToStr(fi.FileVersion[0]));
+             end;
+
+           if [osFileMinorVersion] in outset then
+             begin
+               if verbose then write('File Minor Version:' +#9);
+               writeln(IntToStr(fi.FileVersion[1]));
+             end;
+
+           if [osFileRelease] in outset then
+             begin
+               if verbose then write('File Release:' +#9);
+               writeln(IntToStr(fi.FileVersion[2]));
+             end;
+
+           if [osFileBuild] in outset then
+             begin
+               if verbose then write('File Build:' +#9);
+               writeln(IntToStr(fi.FileVersion[3]));
+             end;
+
+
+           if [osProductFullVersion] in outset then
+             begin
+               if verbose then write('Product Full Version:' +#9);
+               writeln(IntToStr(fi.ProductVersion[0])
+                              + '.' + IntToStr(fi.ProductVersion[1])
+                              + '.' + IntToStr(fi.ProductVersion[2])
+                              + '.' + IntToStr(fi.ProductVersion[3]));
+             end;
+
+           if [osProductMajorVersion] in outset then
+             begin
+               if verbose then write('Product Major Version:' +#9);
+               writeln(IntToStr(fi.ProductVersion[0]));
+             end;
+
+           if [osProductMinorVersion] in outset then
+             begin
+               if verbose then write('Product Minor Version:' +#9);
+               writeln(IntToStr(fi.ProductVersion[1]));
+             end;
+
+           if [osProductRelease] in outset then
+             begin
+               if verbose then write('Product Release:' +#9);
+               writeln(IntToStr(fi.ProductVersion[2]));
+             end;
+
+           if [osProductBuild] in outset then
+             begin
+               if verbose then write('Product Build:' +#9);
+               writeln(IntToStr(fi.ProductVersion[3]));
+             end;
+
+         end;
+        finally
+        end;
+      end;
+
+    // clean up - adding try..except blocks would be nice:
+    resources.Free;
+    reader.Free;
+    inFile.Free;
+    end;
+
 end.
 

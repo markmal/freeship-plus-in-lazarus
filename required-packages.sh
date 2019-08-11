e6a673037c34d0a398b866d72a5c998d8627d2dd
markmal <markmal@github.com>
2019-08-11 10:59:34 -0400

    minor adjustments

--- required-packages.sh
+++ required-packages.sh
@@ -1,33 +1,56 @@
-#shows all packages required for FreeShip binary
-# no params	immediate packages that are not installed
-# -a 		all packages
-# -i 		immediate ones
+function printHelp {
+echo "Shows packages required for FreeShip executable"
+echo "Usage: $0 [-a][-n] [-f <filename>]"
+echo " -a 		show all packages that the executable depends on including"
+echo "    		recursive dependencies. Otherwise it shows only immediate"
+echo "    		dependency packages."
+echo " -n 		show packages that are not installed"
+echo " -f <FileName>	an executable file. Be careful to run this with -a on unknown"
+echo "              	files. A virus can be invoked by ldd."
+echo "              	By default file is FreeShip in local directory."
+echo " -h|-?		Help"
+}
 
+ALL=0; NOI=0; FREESHIP=FreeShip
+while getopts “anf:” opt; do
+  #echo "opt:$opt - arg:$OPTARG"
+  case $opt in
+    a) ALL=1 ;;
+    n) NOI=1 ;;
+    f) FREESHIP=$OPTARG ;;
+    h) printHelp; exit ;;
+    ?) printHelp; exit ;;
+    *) printHelp; exit ;;
+  esac
+done
+
+echo "Executable: $FREESHIP"
+
+ARCH=$(objdump -p $FREESHIP | grep 'file format'|sed 's/.*file format elf..-//')
+if [ "$ARCH" == "x86-64" ]; then ARCH=amd64; fi
+echo "Architecture: $ARCH"
 
 # get list of required libs
 RL=
-if [ "$1" = "-a" ]; 
+if [ $ALL -eq 1 ]; 
  then 
-  for L in $(ldd FreeShip|grep -v 'linux-vdso.so.1'|cut -f2|awk '{print $1}'); do RL="$RL $L"; done
+  for L in $(ldd $FREESHIP | grep -v 'linux-vdso.so.1'|cut -f2|awk '{print $1}'); do RL="$RL $L"; done
+  #echo Libs: $RL
  else
-  for L in $(readelf -d FreeShip|grep " (NEEDED) "|sed 's/^.*\[\(.*\)\].*/\1/'); do RL="$RL $L"; done
-  if [ "$RL" = "" ] ; then echo "All required packages are installed"; exit 0; fi
+  for L in $(objdump -p $FREESHIP|grep " NEEDED " | awk '{print $2}'); do RL="$RL $L"; done
+  #echo Libs: $RL
 fi
-
-
 
 if [ -x "$(which dpkg-query 2>/dev/null)" ]; then
-  RP=$(dpkg-query -S $RL |cut -d: -f1|sort -u)
-  if [ "$1" = "-a" ] || [ "$1" = "-i" ]; 
-    then 
-	echo "All packages that are required: "
-	dpkg-query -l $RP | grep amd64
-    else 
-	echo "All packages that are required: "$RP
+  RP=$(dpkg-query -S $RL | grep ":${ARCH}:" | cut -d: -f1,2|sort -u)
+  echo Required Packages: $RP
+
+  if [ $NOI -eq 1 ]; then
 	echo "Required packages that are not installed: "
-	dpkg-query -l $RP | grep amd64 | grep -v '^ii' ;
+	dpkg-query -l $RP | grep ":$ARCH" | grep -v '^ii' | awk '{print $2}';
   fi
 fi
+
 
 if [ -x "$(which yum 2>/dev/null)" ]; then
   yum whatprovides $RL

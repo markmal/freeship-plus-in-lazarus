be797e5562d4b04e3ddff4d4ab603ad5fef27949
markmal <markmal@github.com>
2020-06-12 19:50:18 -0400

    fixed export 2D DDX Polylines

--- Forms/Free2DDXFExportDlg.pas
+++ Forms/Free2DDXFExportDlg.pas
@@ -33,76 +33,78 @@ interface
 
 uses
     {$IFDEF Windows}
-     Windows,
-     shlobj,
+  Windows,
+  shlobj,
     {$ifdef LCL}
          {$IFDEF VER3}
-      LazUTF8,
-      LazFileUtils,
+  LazUTF8,
+  LazFileUtils,
      {$ELSE}
-      FileUtil, //deprecated
+  FileUtil, //deprecated
      {$ENDIF}
 
     {$endif}
     {$ELSE}
-     LCLIntf, LCLType, 
+  LCLIntf, LCLType,
           {$IFDEF VER3}
-      LazUTF8,
-      LazFileUtils,
+  LazUTF8,
+  LazFileUtils,
      {$ELSE}
-      FileUtil, //deprecated
+  FileUtil, //deprecated
      {$ENDIF}
 
     {$ENDIF}
-     SysUtils,
-     Classes,
-     Graphics,
-     Controls,
-     Forms,
-     Dialogs,
-     StdCtrls,
-     Buttons,
-     ExtCtrls,
-     Spin,
-    FreeShipUnit,FreeLanguageSupport;
-
+  SysUtils,
+  Classes,
+  Graphics,
+  Controls,
+  Forms,
+  Dialogs,
+  StdCtrls,
+  Buttons,
+  ExtCtrls,
+  Spin,
+  FreeShipUnit, FreeLanguageSupport;
 type
 
-{ TDXFExport2DDialog }
+  { TDXFExport2DDialog }
 
- TDXFExport2DDialog = class(TForm)
-                             CheckBox1: TCheckBox;
-                             ComboBox1: TComboBox;
-                             Edit1: TFloatSpinEdit;
-                             Edit3: TEdit;
-                             Label1: TLabel;
-                             Label4: TLabel;
-                             Label7: TLabel;
-                              Panel1: TPanel;
-                              Panel3: TPanel;
-                              Panel4: TPanel;
-                              SaveDialog: TSaveDialog;
-                              BitBtn1: TSpeedButton;
-                              BitBtn2: TSpeedButton;
-                              SpeedButton1: TSpeedButton;
-                              procedure SpeedButton1Click(Sender: TObject);
-                              procedure BitBtn1Click(Sender: TObject);
-                              procedure BitBtn2Click(Sender: TObject);
-                              procedure ComboBox1Change(Sender: TObject);
-                           private   { Private declarations }
-                              function FGetExportDirectory:string;
-                              procedure FSetExportDirectory(val:string);
-                              function FGetSegmentLength:double;
-                              procedure FSetSegmentLength(val:double);
-                              procedure FSetUnits;
-                           public    { Public declarations }
-                              function BrowseForFolder(Const browseTitle: PAnsiChar;initialFolder: String = ''): String;
-                              function Execute:Boolean;
-                              property ExportDirectory   : string read FGetExportDirectory write FSetExportDirectory;
-                              property SegmentLength     : double read FGetSegmentLength write FSetSegmentLength;
-                        end;
+  TDXFExport2DDialog = class(TForm)
+    cbCreateIndividualFiles: TCheckBox;
+    ComboBox1: TComboBox;
+    Edit1: TFloatSpinEdit;
+    Edit3: TEdit;
+    Label1: TLabel;
+    Label4: TLabel;
+    Label7: TLabel;
+    Panel1: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    SaveDialog: TSaveDialog;
+    BitBtn1: TSpeedButton;
+    BitBtn2: TSpeedButton;
+    SpeedButton1: TSpeedButton;
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
+    procedure BitBtn2Click(Sender: TObject);
+    procedure ComboBox1Change(Sender: TObject);
+  private   { Private declarations }
+    function FGetExportDirectory: string;
+    procedure FSetExportDirectory(val: string);
+    function FGetSegmentLength: double;
+    procedure FSetSegmentLength(val: double);
+    procedure FSetUnits;
+  public    { Public declarations }
+    function BrowseForFolder(
+      const browseTitle: PAnsiChar; initialFolder: string = ''): string;
+    function Execute: boolean;
+    property ExportDirectory: string
+      read FGetExportDirectory write FSetExportDirectory;
+    property SegmentLength: double
+      read FGetSegmentLength write FSetSegmentLength;
+  end;
 
-var DXFExport2DDialog:TDXFExport2DDialog;
+var DXFExport2DDialog: TDXFExport2DDialog;
 
 implementation
 
@@ -112,16 +114,18 @@ implementation
   {$R *.lfm}
 {$ENDIF}
 
-var lg_StartFolder: String;
+var lg_StartFolder: string;
 
 {$IFnDEF FPC}
 ///////////////////////////////////////////////////////////////////
 // Call back function used to set the initial browse directory.
 ///////////////////////////////////////////////////////////////////
-function BrowseForFolderCallBack(Wnd: HWND; uMsg: UINT;lParam, lpData: LPARAM): Integer stdcall;
+function BrowseForFolderCallBack(Wnd: HWND;
+  uMsg: UINT; lParam, lpData: LPARAM): integer stdcall;
 begin
-   if uMsg = BFFM_INITIALIZED then SendMessage(Wnd,BFFM_SETSELECTION,1,Integer(@lg_StartFolder[1]));
-   result := 0;
+  if uMsg = BFFM_INITIALIZED then
+    SendMessage(Wnd, BFFM_SETSELECTION, 1, integer(@lg_StartFolder[1]));
+  Result := 0;
 end;{BrowseForFolderCallBack}
 
 ///////////////////////////////////////////////////////////////////
@@ -135,105 +139,108 @@ end;{BrowseForFolderCallBack}
 // Returns: The empty string if no folder was selected (i.e. if the
 //          user clicked cancel), otherwise the full folder path.
 ///////////////////////////////////////////////////////////////////
-function TDXFExport2DDialog.BrowseForFolder(Const browseTitle: PAnsiChar;initialFolder: String=''): String;
-var browse_info   : TBrowseInfo;
-    folder        : array[0..MAX_PATH] of char;
-    find_context  : PItemIDList;
-    I             : Integer;
+function TDXFExport2DDialog.BrowseForFolder(const browseTitle: PAnsiChar;
+  initialFolder: string = ''): string;
+var browse_info: TBrowseInfo;
+  folder: array[0..MAX_PATH] of char;
+  find_context: PItemIDList;
+  I: integer;
 begin
-   FillChar(browse_info,SizeOf(browse_info),#0);
-   lg_StartFolder := initialFolder;
-   browse_info.pszDisplayName := @folder[0];
-   browse_info.lpszTitle:=browseTitle;
-   browse_info.ulFlags := BIF_RETURNONLYFSDIRS;
-   browse_info.hwndOwner := Application.Handle;
-   if initialFolder<>'' then browse_info.lpfn := BrowseForFolderCallBack;
-   find_context := SHBrowseForFolder(browse_info);
-   if Assigned(find_context) then
-   begin
-      if SHGetPathFromIDList(find_context,folder) then
-      begin
-         result:='';
-         for I:=1 to length(Folder) do
-         begin
-            if Folder[I-1]=#0 then break else Result:=result+Folder[I-1];
-         end;
-      end else result := '';
-      GlobalFreePtr(find_context);
-  end else result := '';
+  FillChar(browse_info, SizeOf(browse_info), #0);
+  lg_StartFolder := initialFolder;
+  browse_info.pszDisplayName := @folder[0];
+  browse_info.lpszTitle := browseTitle;
+  browse_info.ulFlags := BIF_RETURNONLYFSDIRS;
+  browse_info.hwndOwner := Application.Handle;
+  if initialFolder <> '' then browse_info.lpfn := BrowseForFolderCallBack;
+  find_context := SHBrowseForFolder(browse_info);
+  if Assigned(find_context) then
+  begin
+    if SHGetPathFromIDList(find_context, folder) then
+    begin
+      Result := '';
+      for I := 1 to length(Folder) do
+        if Folder[I - 1] = #0 then break else Result := Result + Folder[I - 1];
+    end
+    else Result := '';
+    GlobalFreePtr(find_context);
+  end
+  else Result := '';
 end;{TDXFExport2DDialog.BrowseForFolder}
 {$ENDIF}
 
-function TDXFExport2DDialog.BrowseForFolder(Const browseTitle: PAnsiChar;initialFolder: String=''): String;
- var dlg :TSelectDirectoryDialog;
+function TDXFExport2DDialog.BrowseForFolder(const browseTitle: PAnsiChar;
+  initialFolder: string = ''): string;
+var dlg: TSelectDirectoryDialog;
 begin
- dlg := TSelectDirectoryDialog.Create(Self);
- dlg.Title:=browseTitle;
- dlg.InitialDir:=initialFolder;
- if dlg.Execute then
-  Result := dlg.FileName
- else
-  Result := '';
+  dlg := TSelectDirectoryDialog.Create(Self);
+  dlg.Title := browseTitle;
+  dlg.InitialDir := initialFolder;
+  if dlg.Execute then
+    Result := dlg.FileName
+  else
+    Result := '';
 end;
 
-function TDXFExport2DDialog.FGetExportDirectory:string;
+function TDXFExport2DDialog.FGetExportDirectory: string;
 begin
-   Result:=Edit3.Text;
+  Result := Edit3.Text;
 end;{TDXFExport2DDialog.FGetExportDirectory}
 
-procedure TDXFExport2DDialog.FSetExportDirectory(val:string);
+procedure TDXFExport2DDialog.FSetExportDirectory(val: string);
 begin
-   Edit3.Text:=Val;
+  Edit3.Text := Val;
 end;{TDXFExport2DDialog.FSetExportDirectory}
 
-function TDXFExport2DDialog.FGetSegmentLength:double;
+function TDXFExport2DDialog.FGetSegmentLength: double;
 begin
-   Result:=Edit1.Value;
+  Result := Edit1.Value;
 end;{TDXFExport2DDialog.FGetSegmentLength}
 
-procedure TDXFExport2DDialog.FSetSegmentLength(val:double);
+procedure TDXFExport2DDialog.FSetSegmentLength(val: double);
 begin
-   if Val<1e-5 then Val:=1e-5;
-   Edit1.Value:=Val;
+  if Val < 1e-5 then Val := 1e-5;
+  Edit1.Value := Val;
 end;{TDXFExport2DDialog.FSetSegmentLength}
 
 procedure TDXFExport2DDialog.FSetUnits;
-var Str:String;
+var Str: string;
 begin
-   Str:=ComboBox1.Text+Userstring(937);
-   //Label3.Caption:=Str;
+  Str := ComboBox1.Text + Userstring(937);
+  //Label3.Caption:=Str;
 end;{TDXFExport2DDialog.FSetUnits}
 
-function TDXFExport2DDialog.Execute:Boolean;
+function TDXFExport2DDialog.Execute: boolean;
 begin
-   FSetUnits;
-   GlobalFreeship.Preferences.LoadImageIntoBitmap(BitBtn1.Glyph,'Ok');
-   GlobalFreeship.Preferences.LoadImageIntoBitmap(BitBtn2.Glyph,'Cancel');
-   ShowTranslatedValues(Self);
-   Showmodal;
-   Result:=ModalResult=mrOk;
+  FSetUnits;
+  GlobalFreeship.Preferences.LoadImageIntoBitmap(BitBtn1.Glyph, 'Ok');
+  GlobalFreeship.Preferences.LoadImageIntoBitmap(BitBtn2.Glyph, 'Cancel');
+  ShowTranslatedValues(Self);
+  Showmodal;
+  Result := ModalResult = mrOk;
 end;{TDXFExport2DDialog.Execute}
 
 procedure TDXFExport2DDialog.SpeedButton1Click(Sender: TObject);
-var Tmp:string;
+var Tmp: string;
 begin
-   Tmp:=BrowseForFolder(PAnsichar(Userstring(209)+':'),ExportDirectory);
-   if DirectoryExistsUTF8(Tmp) { *Converted from DirectoryExists* } then self.ExportDirectory:=Tmp;
+  Tmp := BrowseForFolder(PAnsichar(Userstring(209) + ':'), ExportDirectory);
+  if DirectoryExistsUTF8(Tmp) { *Converted from DirectoryExists* } then
+    self.ExportDirectory := Tmp;
 end;{TDXFExport2DDialog.SpeedButton1Click}
 
 procedure TDXFExport2DDialog.BitBtn1Click(Sender: TObject);
 begin
-   ModalResult:=mrOk;
+  ModalResult := mrOk;
 end;{TDXFExport2DDialog.BitBtn1Click}
 
 procedure TDXFExport2DDialog.BitBtn2Click(Sender: TObject);
 begin
-   ModalResult:=mrCancel;
+  ModalResult := mrCancel;
 end;{TDXFExport2DDialog.BitBtn2Click}
 
 procedure TDXFExport2DDialog.ComboBox1Change(Sender: TObject);
 begin
-   FSetUnits;
+  FSetUnits;
 end;{TDXFExport2DDialog.ComboBox1Change}
 
 end.
